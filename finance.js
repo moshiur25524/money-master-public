0b6f8686e9856831b97c06019621a1e97708d64d
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-02-18 12:59:01 +0600

    income and expance added

--- finance.js
+++ finance.js
@@ -1,3 +1,46 @@
 document.getElementById('calculation-btn').addEventListener('click', function () {
-    console.log('button is working');
+
+    // Income field value
+
+    const incomeField = document.getElementById('income-value');
+    const incomeFieldAmountText = incomeField.value;
+    const incomeFieldAmount = parseFloat(incomeFieldAmountText);
+    // food cost input
+
+    const foodCost = document.getElementById('food-cost');
+    const foodCostAmountText = foodCost.value;
+    const foodCostAmount = parseFloat(foodCostAmountText);
+
+    // Rent cost input
+
+    const rentCost = document.getElementById('rent-cost');
+    const rentCostAmountText = rentCost.value;
+    const rentCostAmount = parseFloat(rentCostAmountText);
+
+    // Cloth cost input
+
+    const clothCost = document.getElementById('cloth-cost');
+    const clothCostAmountText = clothCost.value;
+    const clothCostAmount = parseFloat(clothCostAmountText);
+
+    // Total expance value
+
+    const expenceTotal = document.getElementById('expence-total');
+    const expenceTotalAmount = foodCostAmount + clothCostAmount + rentCostAmount;
+    expenceTotal.innerText = expenceTotalAmount;
+
+
+    const restTotal = document.getElementById('rest-balance');
+    const restTotalAmount = incomeFieldAmount - expenceTotalAmount;
+    restTotal.innerText = restTotalAmount;
+
+
+
+    // empty the input field
+    foodCost.value = '';
+    rentCost.value = '';
+    clothCost.value = '';
+    incomeField.value = '';
+
 })
+
